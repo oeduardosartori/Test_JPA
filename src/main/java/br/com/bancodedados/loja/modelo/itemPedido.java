2dccdba3ae99e5cf55ce449e2bfec0d42fc830c0
Eduardo <edusartori00@gmail.com>
2022-12-26 11:02:42 -0300

    Criando relacionamento many-to-many

---
+++ src/main/java/br/com/bancodedados/loja/modelo/itemPedido.java
@@ -0,0 +1,49 @@
+package br.com.bancodedados.loja.modelo;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "itens_pedido")
+public class itemPedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal precoUnitario;
+    private int quantidade;
+    @ManyToOne
+    private Pedido pedido;
+    @ManyToOne
+    private Produto produto;
+
+    public itemPedido() {
+    }
+
+    public itemPedido(int quantidade, Pedido pedido, Produto produto) {
+        this.quantidade = quantidade;
+        this.pedido = pedido;
+        this.produto = produto;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public BigDecimal getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+}
