b23c6fa9aa66c9f22dc98260a2687360202b7f90
Eduardo <edusartori00@gmail.com>
2022-12-26 16:35:17 -0300

    Consultas com Criteria API

--- src/main/java/br/com/bancodedados/loja/dao/ProdutoDao.java
+++ src/main/java/br/com/bancodedados/loja/dao/ProdutoDao.java
@@ -1,9 +1,15 @@
 package br.com.bancodedados.loja.dao;
 
 import br.com.bancodedados.loja.modelo.Produto;
+import org.ietf.jgss.ChannelBinding;
 
 import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 public class ProdutoDao {
@@ -55,5 +61,27 @@ public class ProdutoDao {
         return manager.createQuery(jpql, BigDecimal.class)
                 .setParameter("nome", nome)
                 .getSingleResult();
+    }
+
+    public List<Produto> buscaParametrosCriteria(String nome, BigDecimal preco, LocalDate dataCadastro) {
+
+        CriteriaBuilder builder = manager.getCriteriaBuilder();
+        CriteriaQuery<Produto> query = builder.createQuery(Produto.class);
+        Root<Produto> from = query.from(Produto.class);
+
+        Predicate filtros = builder.and();
+
+        if (nome != null && !nome.trim().isEmpty()) {
+            builder.and(filtros, builder.equal(from.get("nome"),nome));
+        }
+        else if (preco != null) {
+            builder.and(filtros, builder.equal(from.get("preco"),preco));
+        }
+        else if (dataCadastro != null) {
+            builder.and(filtros, builder.equal(from.get("dataCadastro"),dataCadastro));
+        }
+        query.where(filtros);
+
+        return manager.createQuery(query).getResultList();
     }
 }
